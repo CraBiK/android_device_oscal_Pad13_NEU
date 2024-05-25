a259e1c371becc086f83fa4ca3cb29c2f32ec823
Andrey <a.kramarev@gmail.com>
2024-05-25 11:16:37 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_Pad13_NEU-user
-add_lunch_combo omni_Pad13_NEU-userdebug
-add_lunch_combo omni_Pad13_NEU-eng
+add_lunch_combo Pad13_NEU-user
+add_lunch_combo Pad13_NEU-userdebug
+add_lunch_combo Pad13_NEU-eng
